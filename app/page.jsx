0d88f49fb2f1a34894489e84c1081d547fe2d7c2
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2023-04-09 13:52:50 +0200

    your massage here

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
 import { Inter } from "@next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 import logo from "../public/image/My project.png";
 import Countdown from "./componnets/countdown";
-import styles from "./styles/page.module.css";
 import "./styles/globals.css";
+import styles from "./styles/page.module.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home(props) {
@@ -22,13 +23,13 @@ export default function Home(props) {
           Check More About Us by Download our Catalog in the button Down below
         </p>
         <button className="p-3 bg-blue-800 flex flex-col justify-center items-center text-white font-mono font-bold">
-          <a
+          <Link
             href="https://drive.google.com/file/d/1JQwy0tlURPNYMH7M5O2WTR1k0rdiUE0X/view?usp=sharing"
             rel="noreferrer"
             target="_blank"
           >
             Download Catalog
-          </a>
+          </Link>
         </button>
       </div>
     </main>
