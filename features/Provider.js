1c1b77325e36666ce14a76524d5262cc56caf12d
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2023-06-20 15:44:25 +0300

    new update

--- features/Provider.js
+++
@@ -1,8 +0,0 @@
-'use client';
-//redux
-import { Provider } from 'react-redux';
-import store from './store';
-
-export function Providers({ children }) {
-  return <Provider store={store}>{children}</Provider>;
-}
