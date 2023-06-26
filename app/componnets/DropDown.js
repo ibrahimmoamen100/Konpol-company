031d271bad87d33a91d5b27b5f806ec86d0b320e
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2023-06-26 15:49:02 +0300

    style dropdown

--- app/componnets/DropDown.js
+++ app/componnets/DropDown.js
@@ -9,18 +9,21 @@ const DropDown = ({ data }) => {
   return (
     <animated.div
       style={spring}
-      className="z-10 bg-white w-screen absolute flex flex-wrap flex-col justify-center items-start top-10 left-0 border-2 border-gray-700"
+      className="z-10 bg-white w-screen absolute  top-12 left-0 border-2 border-gray-200 "
     >
-      <div className="container m-auto">
-        <Link href="/products" className="font-medium text-secondMain p-4">
+      <div className="container m-auto flex flex-wrap flex-col justify-center items-start w-full">
+        <Link
+          href="/products"
+          className="font-medium text-secondMain p-2 border-b-2 border-gray-300 flex-1 w-full"
+        >
           See All Products
         </Link>
 
-        <ul className="flex justify-start h-auto p-2 flex-row gap-12  basis-2/5 ">
+        <ul className="flex flex-wrap flex-row w-full gap-x-16 justify-start ">
           {data.map((e) => (
             <Link
               key={Math.random()}
-              className="p-2 border-b-2 border-gray-200 flex-1"
+              className="p-2 border-b-2 border-gray-200 basis-1/5 flex justify-start items-center"
               href={e.link}
             >
               {e.name}
